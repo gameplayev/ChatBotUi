831ebe99abd26cbdfbc14ee39d3676dcbe3ce5a3
wuseck <gameplayevs@gmail.com>
2025-02-14 00:03:16 +0900

    just test

---
+++ app/compomnets/pages/sidebar/sidebar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import styles from "./Sidebar.module.scss"; // CSS 모듈 가져오기
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      
+    </div>
+  );
+};
+
+export default Sidebar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import style from "./page.module.scss";
 
 export default function Main(){
   return(
